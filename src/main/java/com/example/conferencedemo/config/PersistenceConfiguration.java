890e7422cfa4ab6ef90ab83dd355c2d66fddecf5
ascipio <aniksk@gmail.com>
2020-02-24 18:23:55 +0900

    Persistence commented

--- src/main/java/com/example/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/example/conferencedemo/config/PersistenceConfiguration.java
@@ -8,11 +8,11 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
+/*    @Bean
     public DataSource dataSource(){
         DataSourceBuilder builder = DataSourceBuilder.create();
         builder.url("jdbc:postgresql://localhost:5432/conference_app");
         System.out.println("Custom DataSource by JAVA Configuration");
         return builder.build();
-    }
+    }*/
 }
